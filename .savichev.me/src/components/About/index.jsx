f80d654fc74699576b1f24be78e0f193c50b8ee1
Igor Savichev <igor@savichev.me>
2020-03-26 22:35:19 +0500

    Обновил информацию о себе

--- .savichev.me/src/components/About/index.jsx
+++ .savichev.me/src/components/About/index.jsx
@@ -8,13 +8,30 @@ const About = () => {
             <div className="col-md-8">
                 <h2 className="about__title">Деятельность</h2>
                 <p className="lead">
-                    Я студент 6-го курса&nbsp;
+                    Закончил&nbsp;
                     <a target="_blank" href="http://imkn.urfu.ru/" rel="noopener noreferrer">
-                        математико-механического факультета
+                        математико-механический факультет
                     </a> УрГУ, ныне - <a target="_blank" href="http://urfu.ru/ru/" rel="noopener noreferrer">УрФУ</a>.
+                    <br/>
+                    Обучался по специальности <q>Компьютерная Безопасность</q>.
                 </p>
                 <p className="lead">
-                    Обучаюсь по специальности <q>Компьютерная Безопасность</q>.
+                    Продолжил обучение в магистратуре&nbsp;
+                    <a target="_blank" href="https://rtf.urfu.ru/ru/" rel="noopener noreferrer">ИРИТ-РТФ</a>&nbsp;
+                    <a target="_blank" href="http://urfu.ru/ru/" rel="noopener noreferrer">УрФУ</a>.
+                    <br/>
+                    Обучаюсь по специальности <q>Инженерия программного обеспечения</q>.
+                </p>
+                <p className="lead about__work">
+                    В настоящее время работаю разработчиком в компании&nbsp;
+                    <a target="_blank" href="https://yandex.ru/" rel="noopener noreferrer">
+                        <img
+                            src={require('./images/yandex_logo.svg')}
+                            className="about__logo"
+                            alt="yandex"
+                        />
+                        <span className="about__company">Яндекс.</span>
+                    </a>
                 </p>
             </div>
             <div className="col-md-4">
